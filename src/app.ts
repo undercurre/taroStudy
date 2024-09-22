d2f407dc188086f4fca6f839a337ac0079dd446d
lirhua <undercurre@163.com>
2024-09-22 19:16:36 +0800

    feat：学习生命周期

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,51 @@
-import { PropsWithChildren } from 'react'
-import { useLaunch } from '@tarojs/taro'
+import { PropsWithChildren } from "react";
+import Taro, {
+  useLaunch,
+  useDidShow,
+  useDidHide,
+  useError,
+  usePageNotFound,
+  useUnhandledRejection,
+} from "@tarojs/taro";
 
-import './app.scss'
+// 全局样式
+import "./app.scss";
 
 function App({ children }: PropsWithChildren<any>) {
   useLaunch(() => {
-    console.log('App launched.')
-  })
+    console.log("App launched.");
+  });
+
+  // 对应 onShow
+  useDidShow(() => {
+    console.log("在小程序环境中对应 app 的 onLaunch。");
+  });
+
+  // 对应 onHide
+  useDidHide(() => {
+    console.log("程序切后台时触发。");
+  });
+
+  useError((error) => {
+    console.log("小程序发生脚本错误或 API 调用报错时触发。", error);
+  });
+
+  usePageNotFound(() => {
+    Taro.redirectTo({
+      url: "pages/...",
+    }); // 如果是 tabbar 页面，请使用 Taro.switchTab
+  });
+
+  useUnhandledRejection((res) => {
+    console.log(
+      "小程序有catch的 Promise 拒绝时触发。",
+      res.reason,
+      res.promise
+    );
+  });
 
   // children 是将要会渲染的页面
-  return children
+  return children;
 }
-  
 
-
-export default App
+export default App;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,109 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { View, Text } from "@tarojs/components";
+import Taro, {
+  useAddToFavorites,
+  useDidHide,
+  useDidShow,
+  useLoad,
+  usePageScroll,
+  usePullDownRefresh,
+  useReachBottom,
+  useReady,
+  useResize,
+  useSaveExitState,
+  useShareAppMessage,
+  useShareTimeline,
+  useTabItemTap,
+  useUnload,
+} from "@tarojs/taro";
+import "./index.scss";
 
-export default function Index () {
+export default function Index() {
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
+
+  useUnload(() => {
+    console.log("onUnload");
+  });
+
+  useReady(() => {
+    // 访问小程序渲染层的 DOM 节点。
+    const query = Taro.createSelectorQuery();
+    query
+      .select("#only")
+      .boundingClientRect()
+      .exec((res) => console.log(res, "res"));
+  });
+
+  useDidShow(() => {
+    console.log("Page Show");
+  });
+
+  useDidHide(() => {
+    console.log("Page Hide");
+  });
+
+  usePullDownRefresh(() => {
+    console.log("onPullDownRefresh");
+  });
+
+  useReachBottom(() => {
+    console.log("onReachBottom");
+  });
+
+  usePageScroll((res) => {
+    console.log(res.scrollTop);
+  });
+
+  useAddToFavorites((res) => {
+    // webview 页面返回 webviewUrl
+    console.log("WebviewUrl: ", res.webviewUrl);
+    return {
+      title: "自定义标题",
+      imageUrl: "https://demo.png",
+      query: "name=xxx&age=xxx",
+    };
+  });
+
+  //Button 组件 openType='share'
+  useShareAppMessage((res) => {
+    if (res.from === "button") {
+      // 来自页面内转发按钮
+      console.log(res.target);
+    }
+    return {
+      title: "自定义转发标题",
+      path: "/page/user?id=123",
+    };
+  });
+
+  useShareTimeline(() => {
+    console.log("分享朋友圈");
+    return {};
+  });
+
+  useResize((res) => {
+    console.log(res.size.windowWidth);
+    console.log(res.size.windowHeight);
+  });
+
+  useTabItemTap((item) => {
+    console.log(item.index);
+    console.log(item.pagePath);
+    console.log(item.text);
+  });
+
+  useSaveExitState(() => {
+    const exitState = { myDataField: "myData" }; // 需要保存的数据
+    return {
+      data: exitState,
+      expireTimeStamp: Date.now() + 24 * 60 * 60 * 1000, // 超时时刻
+    };
+  });
 
   return (
-    <View className='index'>
+    <View className="index">
       <Text>Hello world!</Text>
     </View>
-  )
+  );
 }
